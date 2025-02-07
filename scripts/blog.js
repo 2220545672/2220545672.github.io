c5ae721c9e445a401777334ee538b758257fa46a
meixu <meix@yczx.tech>
2025-02-07 17:04:49 +0800

    fix

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,20 +1,9 @@
-function loadBlogPost() {
-    const path = window.location.pathname;
-    if (path.endsWith('.md')) {
-        const mdPath = path;
-        // 修改重定向路径，使用相对路径
-        window.location.href = './post.html?article=' + encodeURIComponent(mdPath);
-    }
-}
-
 function loadMarkdownContent() {
     const urlParams = new URLSearchParams(window.location.search);
     const article = urlParams.get('article');
     
     if (article) {
-        // 移除开头的 ./ 以确保正确获取文件
-        const cleanPath = article.replace(/^\.\//, '');
-        fetch(cleanPath)
+        fetch(article)
             .then(response => {
                 if (!response.ok) {
                     throw new Error('文章加载失败');
