56928d0c1e8fbaf6681c1827a29508646c75f63f
meixu <meix@yczx.tech>
2025-02-08 09:59:23 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -18,4 +18,21 @@ window.onload = function () {
 window.onpopstate = function () {
     const article = new URLSearchParams(window.location.search).get('article');
     article ? loadArticle(article) : showHomePage();
-}; 
+};
+
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    const mainContent = document.getElementById('main-content');
+    
+    if (sidebar.classList.contains('hidden')) {
+        // 显示侧边栏
+        sidebar.classList.remove('hidden');
+        mainContent.classList.remove('ml-0');
+        mainContent.classList.add('ml-64');
+    } else {
+        // 隐藏侧边栏
+        sidebar.classList.add('hidden');
+        mainContent.classList.remove('ml-64');
+        mainContent.classList.add('ml-0');
+    }
+} 
