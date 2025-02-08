78157435a3bf5a0460487933fc98ce85b7e785d5
meixu <meix@yczx.tech>
2025-02-08 10:27:55 +0800

    fix

--- js/articles.js
+++ js/articles.js
@@ -192,11 +192,12 @@ function updateSidebarMenu(articles) {
     const articleLinks = articles.map(article => `
         <li>
             <a href="#" onclick="loadArticle('${article.file}'); return false;" 
-               class="flex items-center p-2 text-gray-900 dark:text-gray-300 rounded-lg hover:bg-gray-100/70 dark:hover:bg-gray-700/70 backdrop-blur-lg transition-all hover:scale-[1.02] group">
+               class="flex items-center p-2 text-gray-900 dark:text-gray-300 rounded-lg hover:bg-blue-500/10 dark:hover:bg-blue-500/20 backdrop-blur-lg transition-all hover:scale-[1.02] hover:translate-x-1 group relative overflow-hidden">
                 <svg class="w-5 h-5 text-gray-500 dark:text-gray-400 transition-all duration-300 group-hover:text-blue-500 dark:group-hover:text-blue-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
                     <path d="M16 14V2a2 2 0 0 0-2-2H2a2 2 0 0 0-2 2v15a3 3 0 0 0 3 3h12a1 1 0 0 0 0-2h-1v-2a2 2 0 0 0 2-2ZM4 2h2v12H4V2Zm8 16H3a1 1 0 0 1 0-2h9v2Z"/>
                 </svg>
-                <span class="ml-3 truncate transition-all duration-300 group-hover:translate-x-1">${article.title}</span>
+                <span class="ml-3 truncate transition-all duration-300">${article.title}</span>
+                <div class="absolute inset-0 border border-blue-500/0 group-hover:border-blue-500/50 dark:group-hover:border-blue-400/50 rounded-lg transition-all duration-300"></div>
             </a>
         </li>
     `).join('');
@@ -204,12 +205,13 @@ function updateSidebarMenu(articles) {
     sidebarMenu.innerHTML = `
         <li>
             <a href="#" onclick="showHomePage(); return false;" 
-               class="flex items-center p-2 text-gray-900 dark:text-gray-300 rounded-lg hover:bg-gray-100/70 dark:hover:bg-gray-700/70 backdrop-blur-lg transition-all hover:scale-[1.02] group">
+               class="flex items-center p-2 text-gray-900 dark:text-gray-300 rounded-lg hover:bg-blue-500/10 dark:hover:bg-blue-500/20 backdrop-blur-lg transition-all hover:scale-[1.02] hover:translate-x-1 group relative overflow-hidden">
                 <svg class="w-5 h-5 text-gray-500 dark:text-gray-400 transition-all duration-300 group-hover:text-blue-500 dark:group-hover:text-blue-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                     <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
                 </svg>
-                <span class="ml-3 transition-all duration-300 group-hover:translate-x-1">首页</span>
+                <span class="ml-3 transition-all duration-300">首页</span>
+                <div class="absolute inset-0 border border-blue-500/0 group-hover:border-blue-500/50 dark:group-hover:border-blue-400/50 rounded-lg transition-all duration-300"></div>
             </a>
         </li>
         ${articleLinks}
