a2b25fd600c8a6f65e0a25eb0dcc0315e3fc42ca
meixu <meix@yczx.tech>
2025-02-08 10:01:59 +0800

    fix

--- js/main.js
+++ js/main.js
@@ -23,16 +23,19 @@ window.onpopstate = function () {
 function toggleSidebar() {
     const sidebar = document.getElementById('sidebar');
     const mainContent = document.getElementById('main-content');
+    const showSidebarBtn = document.getElementById('show-sidebar-btn');
     
     if (sidebar.classList.contains('hidden')) {
         // 显示侧边栏
         sidebar.classList.remove('hidden');
         mainContent.classList.remove('ml-0');
         mainContent.classList.add('ml-64');
+        showSidebarBtn.classList.add('hidden');
     } else {
         // 隐藏侧边栏
         sidebar.classList.add('hidden');
         mainContent.classList.remove('ml-64');
         mainContent.classList.add('ml-0');
+        showSidebarBtn.classList.remove('hidden');
     }
 } 
